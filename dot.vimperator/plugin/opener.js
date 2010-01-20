bce4e66d6199b01723c5b55d9ceeef2f38a49977
eagletmt <eagletmt@gmail.com>
2010-01-21 03:49:31 +0900

    install opener.js

---
+++ dot.vimperator/plugin/opener.js
@@ -0,0 +1,37 @@
+// http://vimperator.g.hatena.ne.jp/voidy21/20100119/1263907211 の改良(?)版
+
+(function () {
+    let U = liberator.plugins.libly.$U;
+
+    function jump (url) {
+        let index = 0;
+        let url = util.stringToURLArray(url).toString();
+        for each ( [,tab] in tabs.browsers ) {
+            if(url == tab.currentURI.spec){
+                tabs.select(index);
+                return true;
+            }
+            ++index;
+        }
+        return false;
+    }
+
+    "open tabopen edit".split(/\s/).forEach(
+      function (name) {
+        let command = commands.get(name);
+        if (!command)
+          return;
+        U.around(
+          command,
+          "action",
+          function (next, args) {
+            let url = args[0].string;
+            if (!(url && jump(url)))
+              return next();
+          },
+          true
+        );
+      }
+    );
+
+})();
