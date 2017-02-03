44b7380725fcb5e7dd9eaf91032890915789d62f
Kohei Suzuki <eagletmt@gmail.com>
2017-02-04 04:42:14 +0900

    Add s shortcuts

--- dot.config/vimfx/config.js
+++ dot.config/vimfx/config.js
@@ -1,6 +1,7 @@
 vimfx.set('prevent_autofocus', true);
 
 vimfx.set('mode.normal.dev', ';');
+vimfx.set('mode.normal.stop', '');
 vimfx.set('mode.normal.scroll_down', 'j <c-e>');
 vimfx.set('mode.normal.scroll_half_page_down', 'J');
 vimfx.set('mode.normal.scroll_half_page_up', 'K');
@@ -18,3 +19,57 @@ vimfx.addKeyOverrides([({hostname, pathname}) => hostname === 'mail.google.com' 
 vimfx.addKeyOverrides([({hostname}) => ['fl.wanko.cc', 'reader.livedwango.com'].includes(hostname), ['j', 'k', 'w', 's', 'a', 'v', 'p', 'o']]);
 vimfx.addKeyOverrides([({hostname, pathname, search}) => hostname === 'www.pixiv.net' && pathname === '/member_illust.php' && search.includes('mode=medium'), ['b', 'v']]);
 vimfx.addKeyOverrides([({hostname, pathname, search}) => hostname === 'www.pixiv.net' && pathname === '/member_illust.php' && search.includes('mode=manga'), ['j', 'k', 'v', 'z', 'b']]);
+
+let {commands} = vimfx.modes.normal;
+
+vimfx.addCommand({
+  name: 'search_tabs',
+  description: 'Search tabs',
+  category: 'tabs',
+  order: commands.focus_location_bar.order + 1,
+}, ({vim}) => {
+  let {gURLBar} = vim.window;
+  commands.focus_location_bar.run({vim});
+  gURLBar.value = '% ';
+  gURLBar.onInput(new vim.window.KeyboardEvent('input'));
+});
+vimfx.set('custom.mode.normal.search_tabs', 'b');
+
+vimfx.addCommand({
+  name: 'open_google',
+  description: 'Open Google',
+  category: 'tabs',
+  order: commands.focus_location_bar.order + 1,
+}, ({vim}) => {
+  let {gURLBar} = vim.window;
+  commands.focus_location_bar.run({vim});
+  gURLBar.value = 'gs ';
+  gURLBar.onInput(new vim.window.KeyboardEvent('input'));
+});
+vimfx.set('custom.mode.normal.open_google', 'gs');
+
+vimfx.addCommand({
+  name: 'open_twitter',
+  description: 'Open Twitter',
+  category: 'tabs',
+  order: commands.focus_location_bar.order + 1,
+}, ({vim}) => {
+  let {gURLBar} = vim.window;
+  commands.focus_location_bar.run({vim});
+  gURLBar.value = 'tw ';
+  gURLBar.onInput(new vim.window.KeyboardEvent('input'));
+});
+vimfx.set('custom.mode.normal.open_twitter', 'sk');
+
+vimfx.addCommand({
+  name: 'open_alc',
+  description: 'Open alc',
+  category: 'tabs',
+  order: commands.focus_location_bar.order + 1,
+}, ({vim}) => {
+  let {gURLBar} = vim.window;
+  commands.focus_location_bar.run({vim});
+  gURLBar.value = 'alc ';
+  gURLBar.onInput(new vim.window.KeyboardEvent('input'));
+});
+vimfx.set('custom.mode.normal.open_alc', 'sa');
