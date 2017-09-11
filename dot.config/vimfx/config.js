d5f519a9c7c8953f548af3b4f9cf71a42e7993ae
Kohei Suzuki <eagletmt@gmail.com>
2017-09-11 23:29:16 +0900

    vimfx: Fix bind in inoreader

--- dot.config/vimfx/config.js
+++ dot.config/vimfx/config.js
@@ -26,7 +26,7 @@ vimfx.set('mode.marks.exit', '<escape> <c-[>');
 
 vimfx.addKeyOverrides([({hostname, pathname}) => hostname === 'mail.google.com' && pathname.startsWith('/mail/'), ['j', 'k', 'n', 'p', 'o', 'u', 'e', 's', '?']]);
 vimfx.addKeyOverrides([({hostname}) => ['fl.wanko.cc', 'reader.livedwango.com'].includes(hostname), ['j', 'k', 'w', 's', 'a', 'v', 'p', 'o']]);
-vimfx.addKeyOverrides([({hostname}) => hostname === 'www.inoreader.com', ['j', 'k', 'n', 'p', 'v', 'b', 'o', 'm', '1', '2', '3', '4', '5', '?']]);
+vimfx.addKeyOverrides([({hostname}) => hostname === 'www.inoreader.com', ['j', 'J', 'k', 'K', 'n', 'p', 'v', 'b', 'o', 'm', '1', '2', '3', '4', '5', '?']]);
 vimfx.addKeyOverrides([({hostname, pathname, search}) => hostname === 'www.pixiv.net' && pathname === '/member_illust.php' && search.includes('mode=medium'), ['b', 'v']]);
 vimfx.addKeyOverrides([({hostname, pathname, search}) => hostname === 'www.pixiv.net' && pathname === '/member_illust.php' && search.includes('mode=manga'), ['j', 'k', 'v', 'z', 'b']]);
 
